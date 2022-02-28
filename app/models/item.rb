b1c92d36911a1f8a6a340a06c59f2aae50f72121
EC2 Default User <ec2-user@ip-172-31-90-16.ec2.internal>
2022-02-28 05:58:20 +0000

    [update]admin レイアウト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-    has_many :cart_items,dependent::destroy
-    has_many :order_details, dependent::destroy
+    has_many :cart_items
+    has_many :order_details
     belongs_to :genre
     
 end
