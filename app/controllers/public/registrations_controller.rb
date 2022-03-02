a8e4461b63e753c9c85edd6d134887fc4899e71d
EC2 Default User <ec2-user@ip-172-31-90-16.ec2.internal>
2022-03-02 16:26:17 +0000

    [Finish]新規登録、ログイン画面]

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  
+  before_action :configure_sign_up_params, if: :devise_controller?
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -39,7 +39,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+   protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -50,6 +50,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
+  def after_sign_up_path_for(resource)
+    customers_path
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
@@ -60,4 +63,14 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,
+                                                       :first_name,
+                                                       :last_name_kana,
+                                                       :first_name_kana,
+                                                       :postal_code,
+                                                       :address,
+                                                       :telephone_number,
+                                                      ])
+  end
 end
