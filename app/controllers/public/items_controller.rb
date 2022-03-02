b66028622b4f011c6019ec9e7ff5fe6ee13e925c
EC2 Default User <ec2-user@ip-172-31-90-16.ec2.internal>
2022-03-02 16:42:19 +0000

    [public]商品一覧途中

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,10 @@
 class Public::ItemsController < ApplicationController
+    def index
+        @items=Item.all
+        @items_count = @items.count
+    end
+    
+    def show
+        @item=Item.find(params[:id])
+    end
 end
