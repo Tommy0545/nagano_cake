b26c974111221bd4d1af6d3033ec1bdf59b86e2b
EC2 Default User <ec2-user@ip-172-31-90-16.ec2.internal>
2022-03-03 03:19:35 +0000

    動的なヘッダーの完成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,31 @@
 class Public::CustomersController < ApplicationController
+    def show
+    @customer = current_customer
+   end
+
+   def edit
+     @customer = current_customer
+   end
+
+   def update
+    @customer = current_customer
+   end
+
+   def quit
+    @customer = current_customer
+   end
+
+   def out
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+   end
+
+
+ private
+  def customer_params
+   params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
+  end
 end
